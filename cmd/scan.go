980e1bb1867c1433ebbbad4e551b331b8fcd321a
myko <awesomeme155@gmail.com>
2022-05-25 05:57:36 +0300

    add scan subcommand and it's action integration

---
+++ cmd/scan.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/mycok/Pscan/scan"
+)
+
+// scanCmd represents the scan command
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "Run a port scan on the hosts",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+
+		ports, err := cmd.Flags().GetIntSlice("ports")
+		if err != nil {
+			return err
+		}
+
+		return scanAction(os.Stdout, hostsFile, ports)
+	},
+}
+
+func scanAction(w io.Writer, hostsFile string, ports []int) error {
+	hl := &scan.HostList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	results := scan.Run(hl, ports)
+
+	return printResults(w, results)
+}
+
+func printResults(w io.Writer, results []scan.Result) error {
+	message := ""
+
+	for _, r := range results {
+		message += fmt.Sprintf("%s:", r.Host)
+
+		if !r.Found {
+			message += " Host not found\n\n"
+
+			continue
+		}
+
+		message += fmt.Sprintln()
+
+		for _, ps := range r.PortStates {
+			message += fmt.Sprintf("\t%d: %s\n", ps.Port, ps.Open)
+		}
+
+		message += fmt.Sprintln()
+	}
+
+	_, err := fmt.Fprintln(w, message)
+
+	return err
+}
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+
+	rootCmd.Flags().IntSliceP("ports", "p", []int{22, 80, 443}, "ports to scan")
+}
